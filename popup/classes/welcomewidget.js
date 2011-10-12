864dd791189408a8e7ab3729e99a0d8d6e2432b7
VASMAN <vasman@hidev.it>
2011-10-12 22:05:54 +0300

    Use callback instead of event, show 'busy' status when logging in to VK

--- popup/classes/welcomewidget.js
+++ popup/classes/welcomewidget.js
@@ -20,20 +20,29 @@
 
 		var ctrl = me.controls = {
 			vkInfoMessage : $('<div/>', {'class':'vk-info-message', text : i18n.getMessage('vkInfoMessage') }),
-			vkLogin : $('<div/>', {'class':'vk-login-button', text : i18n.getMessage('loginToVk'), title:i18n.getMessage('loginToVk') })
+			vkLogin : $('<div/>', {'class':'vk-login-button', text : i18n.getMessage('loginToVk'), title:i18n.getMessage('loginToVk') }),
+            vkWaitingAuth : $('<div/>', {'class':'vk-waiting-auth', text : i18n.getMessage('vkWaitingAuth') })
 		};
 
-		clickable(ctrl.vkLogin, function(){ me.player.vkAuth(); });
+		clickable(ctrl.vkLogin, function(){
 
-		player.bind( {
-			'vkAuthChanged.welcomeWidget' : function onVKAuthChanged() {
-				if(player.vk) {
-					window.location.reload();
-				}
-			}
-		});
+            ctrl.vkInfoMessage.hide();
+            ctrl.vkLogin.hide();
+            ctrl.vkWaitingAuth.show();
 
-		$el.append(ctrl.vkLogin, ctrl.vkInfoMessage);
+            player.vkAuth(function(err, sess){
+                console.log('FINAL CALLBACK', err, sess);
+                if(err) {
+                    ctrl.vkWaitingAuth.hide();
+                    ctrl.vkInfoMessage.show();
+                    ctrl.vkLogin.show();
+                } else {
+                    window.location.reload();
+                }
+            });
+        });
+
+		$el.append(ctrl.vkLogin, ctrl.vkInfoMessage, ctrl.vkWaitingAuth.hide());
 	}
 
 	app.classes.WelcomeWidget = WelcomeWidget;
