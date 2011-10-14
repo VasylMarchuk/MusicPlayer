e625b74f22615c5e0feaf7051aa5107f5c10b96e
Sergii Pravdzivyi <styx@hidev.it>
2011-10-14 18:55:21 +0300

    VKontakte OAuth Dialog pwned

---
+++ inject/vkframe.js
@@ -0,0 +1,36 @@
+//var ohref = location.href;
+//console.log(Object.getOwnPropertyDescriptor(window,'location'));
+//Object.defineProperty(window,'location',{
+//	get:function(){return {};},
+//	set:function(){alert();}
+//});
+////window.addEventListener('onload',function(){
+//	$(window).ready(function(){
+//		console.log('load');
+//		alert();
+//	});
+////});
+
+if(window.location.href.indexOf('vkontakte')!==-1) {
+
+}
+
+parent = false;
+
+document.onreadystatechange = function(){
+	parent = false;
+	document.scripts = [];
+	document.getElementsByTagName('script')[0] = null;
+	document.getElementsByTagName('script')[1] = null;
+	document.getElementsByTagName('script')[2] = null;
+	//$('script','head').remove();
+	var s = document.createElement('script'); s.type = 'text/javascript'; s.async=true;
+	s.innerHTML = 'window.parent=false;console.log(parent,window);';
+	var h = document.getElementsByTagName('script')[0]; h.parentNode.insertBefore(s, h);
+	//delete document.scripts[1];
+	//document.write(document.readyState);
+//	if(document.readyState=='complete') {
+//	}
+//	else {
+//	}
+};
